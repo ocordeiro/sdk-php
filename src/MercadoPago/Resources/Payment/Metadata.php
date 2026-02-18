2d3af8a9d3395e04fac669d03c0d61e8f1505607
Cordeiro <1471463+ocordeiro@users.noreply.github.com>
2026-02-18 16:21:50 -0300

    Add new properties to Metadata
    The MercadoPago\Resources\Payment\Metadata class is currently missing the preapproval_id property, which is returned by the API when a payment is associated with a subscription

--- src/MercadoPago/Resources/Payment/Metadata.php
+++ src/MercadoPago/Resources/Payment/Metadata.php
@@ -7,4 +7,13 @@ class Metadata
 {
     /** Order number. */
     public ?string $order_number;
+
+    /** User type. **/
+    public ?string $user_type;
+
+    /** Preapproval ID. **/
+    public ?string $preapproval_id;
+
+    /** Available tries **/
+    public ?string $available_tries;
 }
